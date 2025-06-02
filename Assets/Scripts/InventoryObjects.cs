7fe7425a86cfd50b69d7f4671aea0af3ca71aa44
LucasGabrielSousaDaSilva <gabrielsousa@unitins.br>
2025-06-02 08:58:53 -0300

    inventory part 1

---
+++ Assets/Scripts/InventoryObjects.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryObjects", menuName = "Scriptable Objects/InventoryObjects")]
+public class InventoryObjects : ScriptableObject
+{
+    
+}
