a698b3f0b5ab3d41931d8b871b4b9dd8d05d53f6
julioarraes42 <128749124+julioarraes42@users.noreply.github.com>
2025-06-02 13:20:01 -0300

    Revert "inventory part 1"
    This reverts commit 7fe7425a86cfd50b69d7f4671aea0af3ca71aa44.

--- Assets/Scripts/InventoryObjects.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "InventoryObjects", menuName = "Scriptable Objects/InventoryObjects")]
-public class InventoryObjects : ScriptableObject
-{
-    
-}
